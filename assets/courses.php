26713f97649902377e74bed13796f4020afd6ab3
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-06-26 14:10:15 +0530

    Create courses.php

---
+++ assets/courses.php
@@ -0,0 +1,320 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="free-educational-responsive-web-template-webEdu">
+	<meta name="author" content="webThemez.com">
+	<title>ANGANWADI</title>
+        <link rel="icon" href="assets/images/favicon.ico" type="image/ico" />
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+				
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right mainNav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="about.php">About</a></li>
+					<li class="active"><a href="courses.php">Courses</a></li>
+					<li><a href="gallery.php">Gallery</a></li>
+					<li><a href="contact.php">Contact</a></li>
+                                        <li><a href="timetable/login.php">Login</a></li>
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</div>
+	<!-- /.navbar -->
+
+		<header id="head" class="secondary">
+            <div class="container">
+                    <h1>Schemes</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing eliras scele!</p>
+                </div>
+    </header>
+
+    
+    <div class="container">
+<h3>Schemes</h3>
+<p>
+The Women & Child Development Department, Kerala functions for ensuring the holistic, physical, psychological, cognitive and emotional developments of women and children 
+</p>
+<br/>
+<ul class="list-unstyled video-list-thumbs row">
+	<li class="col-lg-3 col-sm-4 col-xs-6">
+		<a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+		    
+			<img src="assets/images/kerala.jpg" alt="Barca" class="img-responsive" height="130px" />
+			<h2>A safe and better living environment</h2>
+			<span class="play-button"></span>
+			<span class="duration">03:15</span>
+		</a>
+	</li>
+	<li class="col-lg-3 col-sm-4 col-xs-6">
+		<a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+			<img src="assets/images/mother.jpeg" alt="Barca" class="img-responsive" height="130px" />
+			<h2>most critical and crucial period of the Child’s life.</h2>
+			<span class="play-button"></span>
+			<span class="duration">03:15</span>
+		</a>
+	</li>
+	<li class="col-lg-3 col-sm-4 col-xs-6">
+		<a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+			<img src="assets/images/med.jpeg" alt="Barca" class="img-responsive" height="130px" />
+			<h2> maintained by the Department of Women & Child Development </h2>
+			<span class="play-button"></span>
+			<span class="duration">03:15</span>
+		</a>
+	</li>
+	<li class="col-lg-3 col-md-4 col-sm-4 col-xs-6">
+		<a href="#" title="Claudio Bravo, antes su debut con el Barça en la Liga">
+			<img src="assets/images/hear.jpg" alt="Barca" class="img-responsive" height="130px" />
+			
+			<span class="play-button"></span>
+			<span class="duration">03:15</span>
+		</a>
+	</li> 
+</ul>
+
+</div>
+	<div id="courses">
+		<section class="container">
+			<h3>Development Courses</h3>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="featured-box"> 
+						<div class="text">
+							<h3>Abhayakiranam</h3>
+							With an objective of providing  a safe and better living environment to destitute and Homeless widows. The scheme  provides monthly financial assistance of Rs. 1000 to the close relatives of destitute widows who provide protection and shelter to them.
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="featured-box"> 
+						<div class="text">
+							<h3>First 1000 days</h3>
+							The 1,000 days between a women’s pregnancy  (270 days)  and her child’s 2nd birthday (730 days) are most critical and crucial period of the Child’s life. 
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="featured-box"> 
+						<div class="text">
+							<h3>Kathorth</h3>
+							The District Level Center for Women has been set up at the district level under the Mahila Shakthi Kendra scheme implemented by the Department of Women and Child Development.
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--<div class="row">
+				<div class="col-md-4">
+					<div class="featured-box"> 
+						<div class="text">
+							<h3>Web App Dev</h3>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="featured-box"> 
+						<div class="text">
+							<h3>Data Base</h3>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="featured-box"> 
+						<div class="text">
+							<h3>Mobile App Dev</h3>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</div>
+					</div>
+				</div>
+			</div>-->
+
+		</section>
+	</div>
+    
+	<!-- container -->
+	<div class="container">
+		<div class="row">
+			<!-- Article content -->
+			<section class="col-sm-12 maincontent">
+				<h3>Children</h3>
+				<p>
+				Pre-school years are central from the point of view of child development. In early childhood, development is rapid and environment creates a deep impact on individual when his development is at its fastest. Similarly, the consequences of deprivation are also much greater in the first five years than that could be, if they happened in later years. Pre-school years are crucial for developing proper values and attitudes in children. The foundation of scientific attitudes and proper values such as hard work, national sentiments etc could be developed in children better, if a beginning in this direction is made in early childhood. 
+				</p>
+				<h3>Women</h3>
+				<p>Women's well-being depends on their empowerment social, cultural, political and economical. The Government of Kerala has adopted various strategies to enhance the status of women and empower them to address the negative social and economic impacts. </p>
+			</section>
+		</div>
+	</div>
+	<!-- /container -->
+ <footer id="footer">
+ 
+		<div class="container">
+   <div class="row">
+  <div class="footerbottom">
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Course Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="#">
+                List of Technology 
+              </a>
+            </li>
+            <li><a href="#">
+                List of Business
+              </a>
+            </li>
+            <li><a href="#">
+                List of Photography
+              </a>
+            </li>
+            <li><a href="#">
+               List of Language
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Products Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li> <a href="#">
+                Individual Plans  </a>
+            </li>
+            <li><a href="#">
+                Business Plans
+              </a>
+            </li>
+            <li><a href="#">
+                Free Trial
+              </a>
+            </li>
+            <li><a href="#">
+                Academic
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Browse by Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="#">
+                All Courses
+              </a>
+            </li>
+            <li> <a href="#">
+                All Instructors
+              </a>
+            </li>
+            <li><a href="#">
+                All Members
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                All Groups
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6"> 
+            	<div class="footerwidget"> 
+                         <h4>Contact</h4> 
+                        <p>Lorem reksi this dummy text unde omnis iste natus error sit volupum</p>
+            <div class="contact-info"> 
+            <i class="fa fa-map-marker"></i> Kerniles 416  - United Kingdom<br>
+            <i class="fa fa-phone"></i>+00 123 156 711 <br>
+             <i class="fa fa-envelope-o"></i> youremail@email.com
+              </div> 
+                </div><!-- end widget --> 
+    </div>
+  </div>
+</div>
+			<div class="social text-center">
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-dribbble"></i></a>
+				<a href="#"><i class="fa fa-flickr"></i></a>
+				<a href="#"><i class="fa fa-github"></i></a>
+			</div>
+
+			<div class="clear"></div>
+			<!--CLEAR FLOATS-->
+		</div>
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="simplenav">
+								<a href="index.html">Home</a> | 
+								<a href="about.html">About</a> |
+								<a href="courses.html">Courses</a> |
+								<a href="price.html">Price</a> |
+								<a href="videos.html">Videos</a> |
+								<a href="contact.html">Contact</a>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="text-right">
+								Copyright &copy; 2014. Template by <a href="http://webthemez.com/" rel="develop">WebThemez.com</a>
+							</p>
+						</div>
+					</div>
+
+				</div>
+				<!-- /row of panels -->
+			</div>
+		</div>
+	</footer>
+
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+</html>
