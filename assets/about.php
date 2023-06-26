0816c0faab238b342a1e9c3db533c5f05e7fa2a3
Gayathri <83536175+GAYATHRI-2002@users.noreply.github.com>
2023-06-26 14:07:57 +0530

    Create about.php

---
+++ assets/about.php
@@ -0,0 +1,347 @@
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="free-educational-responsive-web-template-webEdu">
+    <meta name="author" content="webThemez.com">
+    <title>ANGANWADI</title>
+    <link rel="icon" href="assets/images/favicon.ico" type="image/ico" />
+    <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <!-- Custom styles for our template -->
+    <link rel="stylesheet" href="assets/css/bootstrap-theme.css" media="screen">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <!-- Button for smallest screens -->
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav pull-right mainNav">
+                   <li ><a href="index.php">Home</a></li>
+					<li class="active"><a href="about.php">About</a></li>
+					<li><a href="courses.php">Courses</a></li>
+					<li><a href="videos.php">Gallery</a></li>
+					<li><a href="contact.php">Contact</a></li>
+                                        <li><a href="timetable/login.php">Login</a></li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+    <!-- /.navbar -->
+
+ 	<header id="head" class="secondary">
+            <div class="container">
+                    <h1>About Us</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing eliras scele!</p>
+                </div>
+    </header>
+
+
+    <!-- container -->
+    <section class="container">
+        <div class="row">
+            <!-- main content -->
+            <section class="col-sm-8 maincontent">
+                <h3>About Us</h3>
+                <p>
+                    <img src="assets/images/anga.webp" alt="" class="img-rounded pull-right" width="300">
+                    Children are one of the most beautiful creatures on earth – they are full of joy, energy and curiosity. They constantly explore everything around them and in the process learn many things. The process of exploration and learning in early childhood is crucial to the child’s development. Research studies show that 50% of the total intellectual development of a child is completed by the time he is four years old. Obviously, much care must be provided to the child during its first few years.
+                </p>
+                <p>It is the responsibility of the parents and the society to provide an enriching environment for children, so as to ensure proper development of their innate talents and skills to the maximum possible extent.</p>
+                <h3>Our Achievements</h3>
+                <strong>Nutrition including Supplementary Nutrition</strong>
+                <p>This includes supplementary feeding and growth monitoring; and prophylaxis against vitamin A deficiency and control of nutritional anaemia. All families in the community are surveyed, to identify children below the age of six and pregnant & nursing mothers. They avail of supplementary feeding support for 300 days in a year. By providing supplementary feeding, the Anganwadi attempts to bridge the caloric gap between the national recommended and average intake of children and women in low income and disadvantaged communities. </p>
+                <strong>Immunization</strong>
+                <p>Immunization of pregnant women and infants protects children from six vaccine preventable diseases-poliomyelitis, diphtheria, pertussis, tetanus, tuberculosis and measles. These are major preventable causes of child mortality, disability, morbidity and related malnutrition. Immunization of pregnant women against tetanus also reduces maternal and neonatal mortality.</p>
+                <strong>Health Check-ups</strong>
+                <p>This includes health care of children less than six years of age, antenatal care of expectant mothers and postnatal care of nursing mothers. The various health services provided for children by anganwadi workers and Primary Health Centre (PHC) staff, include regular health check-ups, recording of weight, immunization, management of malnutrition, treatment of diarrhoea, de-worming and distribution of simple medicines etc.</p>
+                <strong>Non-formal Pre-School Education (PSE) </strong>
+                <p>The Non-formal Pre-school Education (PSE) component of the ICDS may well be considered the backbone of the ICDS programme, since all its services essentially converge at the anganwadi – a village courtyard. Anganwadi Centre (AWC) – a village courtyard – is the main platform for delivering of these services. These AWCs have been set up in every village in the country.</p>
+            </section>
+            <!-- /main -->
+
+            <!-- Sidebar -->
+            <aside class="col-sm-4 sidebar sidebar-right">
+
+                <div class="panel">
+                    <h4>Latest News</h4>
+                    <ul class="list-unstyled list-spaces">
+                        <li><a href="">Beti Bachao Beti Padhao</a><br>
+                            <span class="small text-muted">The aim of Beti Bachao Beti Padhao scheme is to stop the drop in girl child sex ratio. </span></li>
+                        <li><a href="">Kaaval Plus</a><br>
+                            <span class="small text-muted">KAVAL PLUS is an innovative program by the Department of Women and Child Development, Government of Kerala. </span></li>
+                        <li><a href="">Juvenile Justice</a><br>
+                            <span class="small text-muted">Every child needs nurturing, education and exercise. These needs don’t end when children come in contact with the law. </span></li>
+                        <li><a href="">Child Line</a><br>
+                            <span class="small text-muted">CHILDLINE is a national emergency toll free phone out reach service for the children in need of care and protection operates on the 1098 number, and is supported by the Ministry of Women and Child Dev....</span></li>
+                        <li><a href="">Child Development</a><br>
+                            <span class="small text-muted">The future of our nation lies definitely on the health and wealth of its children.</span></li>
+                    </ul>
+                </div>
+
+            </aside>
+            <!-- /Sidebar -->
+
+        </div>
+    </section>
+    <!-- /container -->
+    <section class="team-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3> Objectives</h3>
+                    <p>Integrated Child Development Scheme ICDS represents one of the world’s largest and most unique programmes for early childhood development. ICDS is the foremost symbol of India’s commitment to her children – India’s response to the challenge of providing pre-school education on one hand and breaking the vicious cycle of malnutrition, morbidity, reduced learning capacity and mortality, on the other.</p>
+                    <br />
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-md-3 col-sm-6 col-xs-6">
+                    <!-- Team Member -->
+                    <div class="team-member">
+                        <!-- Image Hover Block -->
+                        <div class="member-img">
+                            <!-- Image  -->
+                            <img class="img-responsive" src="assets/images/people1.jpeg" alt="">
+                        </div>
+                        <!-- Member Details -->
+                        <h4>Arjun Hari</h4>
+                        <!-- Designation -->
+                        <span class="pos">CEO</span>
+                        <div class="team-socials">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-dribbble"></i></a>
+                            <a href="#"><i class="fa fa-github"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-6">
+                    <!-- Team Member -->
+                    <div class="team-member pDark">
+                        <!-- Image Hover Block -->
+                        <div class="member-img">
+                            <!-- Image  -->
+                            <img class="img-responsive" src="assets/images/people2.jpeg" alt="">
+                        </div>
+                        <!-- Member Details -->
+                        <h4>Rajeev Kumar</h4>
+                        <!-- Designation -->
+                        <span class="pos">Director</span>
+                        <div class="team-socials">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-dribbble"></i></a>
+                            <a href="#"><i class="fa fa-github"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-6">
+                    <!-- Team Member -->
+                    <div class="team-member pDark">
+                        <!-- Image Hover Block -->
+                        <div class="member-img">
+                            <!-- Image  -->
+                            <img class="img-responsive" src="assets/images/people3.webp" alt="">
+                        </div>
+                        <!-- Member Details -->
+                        <h4>Sreeja Ravi</h4>
+                        <!-- Designation -->
+                        <span class="pos">Manager</span>
+                        <div class="team-socials">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-dribbble"></i></a>
+                            <a href="#"><i class="fa fa-github"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-6">
+                    <!-- Team Member -->
+                    <div class="team-member pDark">
+                        <!-- Image Hover Block -->
+                        <div class="member-img">
+                            <!-- Image  -->
+                            <img class="img-responsive" src="assets/images/people4.jpg" alt="">
+                        </div>
+                        <!-- Member Details -->
+                        <h4>Jaya</h4>
+                        <!-- Designation -->
+                        <span class="pos">Creative</span>
+                        <div class="team-socials">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-dribbble"></i></a>
+                            <a href="#"><i class="fa fa-github"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  <footer id="footer">
+ 
+		<div class="container">
+   <div class="row">
+  <div class="footerbottom">
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Course Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="#">
+                List of Technology 
+              </a>
+            </li>
+            <li><a href="#">
+                List of Business
+              </a>
+            </li>
+            <li><a href="#">
+                List of Photography
+              </a>
+            </li>
+            <li><a href="#">
+               List of Language
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Products Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li> <a href="#">
+                Individual Plans  </a>
+            </li>
+            <li><a href="#">
+                Business Plans
+              </a>
+            </li>
+            <li><a href="#">
+                Free Trial
+              </a>
+            </li>
+            <li><a href="#">
+                Academic
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Browse by Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="#">
+                All Courses
+              </a>
+            </li>
+            <li> <a href="#">
+                All Instructors
+              </a>
+            </li>
+            <li><a href="#">
+                All Members
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                All Groups
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6"> 
+            	<div class="footerwidget"> 
+                         <h4>Contact</h4> 
+                        <p>Lorem reksi this dummy text unde omnis iste natus error sit volupum</p>
+            <div class="contact-info"> 
+            <i class="fa fa-map-marker"></i> Kerniles 416  - United Kingdom<br>
+            <i class="fa fa-phone"></i>+00 123 156 711 <br>
+             <i class="fa fa-envelope-o"></i> youremail@email.com
+              </div> 
+                </div><!-- end widget --> 
+    </div>
+  </div>
+</div>
+			<div class="social text-center">
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-facebook"></i></a>
+				<a href="#"><i class="fa fa-dribbble"></i></a>
+				<a href="#"><i class="fa fa-flickr"></i></a>
+				<a href="#"><i class="fa fa-github"></i></a>
+			</div>
+
+			<div class="clear"></div>
+			<!--CLEAR FLOATS-->
+		</div>
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="simplenav">
+								<a href="index.html">Home</a> | 
+								<a href="about.html">About</a> |
+								<a href="courses.html">Courses</a> |
+								<a href="price.html">Price</a> |
+								<a href="videos.html">Videos</a> |
+								<a href="contact.html">Contact</a>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="text-right">
+								Copyright &copy; 2014. Template by <a href="http://webthemez.com/" rel="develop">WebThemez.com</a>
+							</p>
+						</div>
+					</div>
+
+				</div>
+				<!-- /row of panels -->
+			</div>
+		</div>
+	</footer>
+
+    <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="assets/js/custom.js"></script>
+</body>
+</html>
